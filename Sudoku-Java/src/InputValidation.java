7d64a629bb44d6cd08a1e60ecde163bb8fa3ed69
Sandro Wehrhahn <mail.sandrowehrhahn@gmail.com>
2018-11-29 21:24:34 +0100

    Delete old function

--- Sudoku-Java/src/InputValidation.java
+++ Sudoku-Java/src/InputValidation.java
@@ -1,29 +1,11 @@
 
 
 public class InputValidation {
-
-	public boolean validateValue(String value) {
-		boolean isValid = false;
-
-		for (int i = 1; i <= 9; i++) {
-			String compareString = Integer.toString(i);
-			
-			if (value.equals(compareString)) {
-				isValid = true;
-				break;
-			}
-			
-		}
-		System.out.println("Value is 1-9 " + isValid);
-		return isValid;
-	}
-
 	
 	// alternative solution using a regular expression (regex)
 	// not tested
 	public boolean validateValueWithRegex(String value) {
-		String regularExpression = "^[1-9]";
-		boolean isValid = value.matches(regularExpression);
+		boolean isValid = value.matches("^[1-9]");
 		System.out.println("Value is 1-9 " + isValid);
 		return isValid;
 	}
